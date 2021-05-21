d6db37eab87af3312de09f88c118d7f64972e771
ySheretz <76839259+ySheretz@users.noreply.github.com>
2021-05-21 11:41:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import ssl
+from flask import Flask
+from flask.globals import request
+from flask.templating import render_template
+from pymongo import MongoClient
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient('mongodb+srv://yuval:yyuuvvaall@microblog-application.tixpn.mongodb.net/test', ssl_cert_reqs=ssl.CERT_NONE)
+    app.db = client.microblog
+
+
+    @app.route("/", methods=["GET", 'POST'])
+    def home():
+        for e in app.db.entries.find({}):
+            print(e)
+
+        if request.method == 'POST':
+            entry_content = request.form.get('content')
+            formatted_date = datetime.today().strftime('%Y-%m-%d')
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+
+        entries_with_dates = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.strptime(entry["date"], '%Y-%m-%d').strftime("%b, %d")
+            )
+            for entry in app.db.entries.find()
+        ]
+
+        return render_template("home.html", entries=entries_with_dates)
+
+    return app
